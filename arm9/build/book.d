05619fd52c7184d6c25bccac9c2da78190d662ab
Vladimir Kosickij <vladimir.kosickij@gmail.com>
2023-02-22 16:02:25 +0300

    Initial commit

---
+++ arm9/build/book.d
@@ -0,0 +1,162 @@
+book.o: C:/Code/IkuReader/arm9/source/book.cpp \
+ c:\code\ikureader\include\book.h c:\code\ikureader\include\default.h \
+ C:/devkitPro/libnds/include/nds.h \
+ C:/devkitPro/libnds/include/nds/libversion.h \
+ C:/devkitPro/libnds/include/nds/ndstypes.h \
+ C:/devkitPro/libnds/include/nds/bios.h \
+ C:/devkitPro/libnds/include/nds/ndstypes.h \
+ C:/devkitPro/libnds/include/nds/card.h \
+ C:/devkitPro/libnds/include/nds/ndstypes.h \
+ C:/devkitPro/libnds/include/nds/debug.h \
+ C:/devkitPro/libnds/include/nds/dma.h \
+ C:/devkitPro/libnds/include/nds/interrupts.h \
+ C:/devkitPro/libnds/include/nds/ipc.h \
+ C:/devkitPro/libnds/include/nds/memory.h \
+ C:/devkitPro/libnds/include/nds/system.h \
+ C:/devkitPro/libnds/include/nds/timers.h \
+ C:/devkitPro/libnds/include/nds/fifocommon.h \
+ C:/devkitPro/libnds/include/nds/interrupts.h \
+ C:/devkitPro/libnds/include/nds/touch.h \
+ C:/devkitPro/libnds/include/nds/input.h \
+ C:/devkitPro/libnds/include/nds/sha1.h \
+ C:/devkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ C:/devkitPro/libnds/include/nds/arm9/linkedlist.h \
+ C:/devkitPro/libnds/include/nds/arm9/background.h \
+ C:/devkitPro/libnds/include/nds/arm9/video.h \
+ C:/devkitPro/libnds/include/nds/arm9/sassert.h \
+ C:/devkitPro/libnds/include/nds/memory.h \
+ C:/devkitPro/libnds/include/nds/dma.h \
+ C:/devkitPro/libnds/include/nds/arm9/boxtest.h \
+ C:/devkitPro/libnds/include/nds/arm9/videoGL.h \
+ C:/devkitPro/libnds/include/nds/arm9/cache.h \
+ C:/devkitPro/libnds/include/nds/arm9/trig_lut.h \
+ C:/devkitPro/libnds/include/nds/arm9/math.h \
+ C:/devkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ C:/devkitPro/libnds/include/nds/arm9/cache.h \
+ C:/devkitPro/libnds/include/nds/arm9/console.h \
+ C:/devkitPro/libnds/include/nds/arm9/background.h \
+ C:/devkitPro/libnds/include/nds/arm9/decompress.h \
+ C:/devkitPro/libnds/include/nds/bios.h \
+ C:/devkitPro/libnds/include/nds/arm9/exceptions.h \
+ C:/devkitPro/libnds/include/nds/arm9/guitarGrip.h \
+ C:/devkitPro/libnds/include/nds/arm9/image.h \
+ C:/devkitPro/libnds/include/nds/arm9/pcx.h \
+ C:/devkitPro/libnds/include/nds/arm9/input.h \
+ C:/devkitPro/libnds/include/nds/touch.h \
+ C:/devkitPro/libnds/include/nds/input.h \
+ C:/devkitPro/libnds/include/nds/arm9/keyboard.h \
+ C:/devkitPro/libnds/include/nds/arm9/math.h \
+ C:/devkitPro/libnds/include/nds/arm9/paddle.h \
+ C:/devkitPro/libnds/include/nds/arm9/pcx.h \
+ C:/devkitPro/libnds/include/nds/arm9/piano.h \
+ C:/devkitPro/libnds/include/nds/arm9/rumble.h \
+ C:/devkitPro/libnds/include/nds/arm9/sassert.h \
+ C:/devkitPro/libnds/include/nds/arm9/sound.h \
+ C:/devkitPro/libnds/include/nds/arm9/sprite.h \
+ C:/devkitPro/libnds/include/nds/system.h \
+ C:/devkitPro/libnds/include/nds/arm9/window.h \
+ C:/devkitPro/libnds/include/nds/arm9/sprite.h \
+ C:/devkitPro/libnds/include/nds/arm9/trig_lut.h \
+ C:/devkitPro/libnds/include/nds/arm9/video.h \
+ C:/devkitPro/libnds/include/nds/arm9/videoGL.h \
+ C:/devkitPro/libnds/include/nds/arm9/nand.h \
+ c:\code\ikureader\include\ndsx_brightness.h \
+ c:\code\ikureader\include\freetype2\ft2build.h \
+ c:\code\ikureader\include\freetype2\freetype\config\ftheader.h \
+ c:\code\ikureader\include\freetype2\freetype\freetype.h \
+ c:\code\ikureader\include\freetype2\freetype\config\ftconfig.h \
+ c:\code\ikureader\include\freetype2\freetype\config\ftoption.h \
+ c:\code\ikureader\include\freetype2\freetype\config\ftstdlib.h \
+ c:\code\ikureader\include\freetype2\freetype\fterrors.h \
+ c:\code\ikureader\include\freetype2\freetype\ftmoderr.h \
+ c:\code\ikureader\include\freetype2\freetype\fterrdef.h \
+ c:\code\ikureader\include\freetype2\freetype\fttypes.h \
+ c:\code\ikureader\include\freetype2\freetype\ftsystem.h \
+ c:\code\ikureader\include\freetype2\freetype\ftimage.h \
+ c:\code\ikureader\include\button.h c:\code\ikureader\include\base64.h \
+ c:\code\ikureader\include\screens.h c:\code\ikureader\include\settings.h \
+ c:\code\ikureader\include\renderer.h \
+ c:\code\ikureader\include\controls.h \
+ c:\code\ikureader\include\encoding_tables.h
+c:\code\ikureader\include\book.h:
+c:\code\ikureader\include\default.h:
+C:/devkitPro/libnds/include/nds.h:
+C:/devkitPro/libnds/include/nds/libversion.h:
+C:/devkitPro/libnds/include/nds/ndstypes.h:
+C:/devkitPro/libnds/include/nds/bios.h:
+C:/devkitPro/libnds/include/nds/ndstypes.h:
+C:/devkitPro/libnds/include/nds/card.h:
+C:/devkitPro/libnds/include/nds/ndstypes.h:
+C:/devkitPro/libnds/include/nds/debug.h:
+C:/devkitPro/libnds/include/nds/dma.h:
+C:/devkitPro/libnds/include/nds/interrupts.h:
+C:/devkitPro/libnds/include/nds/ipc.h:
+C:/devkitPro/libnds/include/nds/memory.h:
+C:/devkitPro/libnds/include/nds/system.h:
+C:/devkitPro/libnds/include/nds/timers.h:
+C:/devkitPro/libnds/include/nds/fifocommon.h:
+C:/devkitPro/libnds/include/nds/interrupts.h:
+C:/devkitPro/libnds/include/nds/touch.h:
+C:/devkitPro/libnds/include/nds/input.h:
+C:/devkitPro/libnds/include/nds/sha1.h:
+C:/devkitPro/libnds/include/nds/arm9/dynamicArray.h:
+C:/devkitPro/libnds/include/nds/arm9/linkedlist.h:
+C:/devkitPro/libnds/include/nds/arm9/background.h:
+C:/devkitPro/libnds/include/nds/arm9/video.h:
+C:/devkitPro/libnds/include/nds/arm9/sassert.h:
+C:/devkitPro/libnds/include/nds/memory.h:
+C:/devkitPro/libnds/include/nds/dma.h:
+C:/devkitPro/libnds/include/nds/arm9/boxtest.h:
+C:/devkitPro/libnds/include/nds/arm9/videoGL.h:
+C:/devkitPro/libnds/include/nds/arm9/cache.h:
+C:/devkitPro/libnds/include/nds/arm9/trig_lut.h:
+C:/devkitPro/libnds/include/nds/arm9/math.h:
+C:/devkitPro/libnds/include/nds/arm9/dynamicArray.h:
+C:/devkitPro/libnds/include/nds/arm9/cache.h:
+C:/devkitPro/libnds/include/nds/arm9/console.h:
+C:/devkitPro/libnds/include/nds/arm9/background.h:
+C:/devkitPro/libnds/include/nds/arm9/decompress.h:
+C:/devkitPro/libnds/include/nds/bios.h:
+C:/devkitPro/libnds/include/nds/arm9/exceptions.h:
+C:/devkitPro/libnds/include/nds/arm9/guitarGrip.h:
+C:/devkitPro/libnds/include/nds/arm9/image.h:
+C:/devkitPro/libnds/include/nds/arm9/pcx.h:
+C:/devkitPro/libnds/include/nds/arm9/input.h:
+C:/devkitPro/libnds/include/nds/touch.h:
+C:/devkitPro/libnds/include/nds/input.h:
+C:/devkitPro/libnds/include/nds/arm9/keyboard.h:
+C:/devkitPro/libnds/include/nds/arm9/math.h:
+C:/devkitPro/libnds/include/nds/arm9/paddle.h:
+C:/devkitPro/libnds/include/nds/arm9/pcx.h:
+C:/devkitPro/libnds/include/nds/arm9/piano.h:
+C:/devkitPro/libnds/include/nds/arm9/rumble.h:
+C:/devkitPro/libnds/include/nds/arm9/sassert.h:
+C:/devkitPro/libnds/include/nds/arm9/sound.h:
+C:/devkitPro/libnds/include/nds/arm9/sprite.h:
+C:/devkitPro/libnds/include/nds/system.h:
+C:/devkitPro/libnds/include/nds/arm9/window.h:
+C:/devkitPro/libnds/include/nds/arm9/sprite.h:
+C:/devkitPro/libnds/include/nds/arm9/trig_lut.h:
+C:/devkitPro/libnds/include/nds/arm9/video.h:
+C:/devkitPro/libnds/include/nds/arm9/videoGL.h:
+C:/devkitPro/libnds/include/nds/arm9/nand.h:
+c:\code\ikureader\include\ndsx_brightness.h:
+c:\code\ikureader\include\freetype2\ft2build.h:
+c:\code\ikureader\include\freetype2\freetype\config\ftheader.h:
+c:\code\ikureader\include\freetype2\freetype\freetype.h:
+c:\code\ikureader\include\freetype2\freetype\config\ftconfig.h:
+c:\code\ikureader\include\freetype2\freetype\config\ftoption.h:
+c:\code\ikureader\include\freetype2\freetype\config\ftstdlib.h:
+c:\code\ikureader\include\freetype2\freetype\fterrors.h:
+c:\code\ikureader\include\freetype2\freetype\ftmoderr.h:
+c:\code\ikureader\include\freetype2\freetype\fterrdef.h:
+c:\code\ikureader\include\freetype2\freetype\fttypes.h:
+c:\code\ikureader\include\freetype2\freetype\ftsystem.h:
+c:\code\ikureader\include\freetype2\freetype\ftimage.h:
+c:\code\ikureader\include\button.h:
+c:\code\ikureader\include\base64.h:
+c:\code\ikureader\include\screens.h:
+c:\code\ikureader\include\settings.h:
+c:\code\ikureader\include\renderer.h:
+c:\code\ikureader\include\controls.h:
+c:\code\ikureader\include\encoding_tables.h:
